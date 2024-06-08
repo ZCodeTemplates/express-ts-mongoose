d8b355511795de6e750cd0fe608ef05077eec79d
eliyamelamed1 <eliyamelamed1@email.com>
2024-06-08 12:34:23 +0300

    feature: added axiosInsatnce and improved typing of wrapAsync

--- src/common/middlewares/wrapAsync.ts
+++ src/common/middlewares/wrapAsync.ts
@@ -1,8 +1,12 @@
-export const wrapAsync = (fn: any) => {
-    return async (req: any, res: any, next: any) => {
+import type { Request, Response, NextFunction } from 'express';
+
+const STATUS_CODE_OK = 200;
+
+export const wrapAsync = (fn: (...args: [Request, Response, NextFunction]) => unknown) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
         try {
             const result = await fn(req, res, next);
-            res.status(200).send(result);
+            res.status(STATUS_CODE_OK).send(result);
         } catch (error) {
             next(error);
         }
